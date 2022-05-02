f76eb42c4a8ee241a121c391bee30e48bb60449a
YuriiSakharuk <mr.sakharukyurii@gmail.com>
2022-05-02 22:26:24 +0300

    gitignore added

--- src/ArraySample.java
+++ src/ArraySample.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 
 public class ArraySample {
-    private int [] arr = new int [7];
-    public void setArray () {
+    private int[] arr = new int[7];
+
+    public void setArray() {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < arr.length; i++) {
-            arr [i] = scanner.nextInt();
+            arr[i] = scanner.nextInt();
         }
     }
-    public int [] getArr () {
+
+    public int[] getArr() {
         return arr;
     }
-    public int sum (int a, int b) {
+
+    public int sum(int a, int b) {
         int sum = a + b;
         return sum;
+    }
+
+    public void equalsTwo(int a) {
+        if (a % 2 == 0)
+            System.out.println(a + " кратне 2");
+        else
+            System.out.println(a + " не кратне 2");
     }
 }
